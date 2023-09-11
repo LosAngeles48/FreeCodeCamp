9af1e989897cb6bbbb41f721e59f27d6ece95035
Dmitry48 <Dima_1996.33@mail.ru>
2023-09-11 20:06:53 +0700

    Define an HTML Class in JSX

---
+++ React/Define an HTML Class in JSX.tsx
@@ -0,0 +1,14 @@
+// Было
+
+const JSX = (
+    <div>
+        <h1>Add a class to this div</h1>
+    </div>
+);
+
+// Стало
+const JSX = (
+    <div className="myDiv">
+        <h1>Add a class to this div</h1>
+    </div>
+);
