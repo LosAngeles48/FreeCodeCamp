caa3719e043546e22fcac07191b3bbe8d21986de
Dmitry48 <Dima_1996.33@mail.ru>
2023-09-18 18:51:00 +0700

    Create a Stateless Functional Component

---
+++ React/Create a Stateless Functional Component.tsx
@@ -0,0 +1,16 @@
+// Было
+const MyComponent = function() {
+    // Change code below this line
+
+    // Change code above this line
+}
+
+
+// Стало
+
+const MyComponent = function() {
+    return (
+        <div> Hello</div>
+    )
+
+}
